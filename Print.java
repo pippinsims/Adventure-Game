2cfd65508f301835e0c15a82c116a212a30acf47
Sam <kuhnssamuel@gmail.com>
2025-01-30 15:08:29 -0500

    Made changes to "Print.java"

--- Print.java
+++ Print.java
@@ -2,6 +2,11 @@ public class Print {
 
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World.");
+        int i = 0;
+        for(int j = 0; j < 1; j++)
+        {
+            System.out.println(j);
+        }
     }
 }
