a9df9e683effddf951cbeddd6f304f6796f4aa80
Sam <kuhnssamuel@gmail.com>
2025-01-30 15:13:20 -0500

    Reverted "Print.java" to it's original state.

--- Print.java
+++ Print.java
@@ -2,11 +2,6 @@ public class Print {
 
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World.");
-        int i = 0;
-        for(int j = 0; j < 1; j++)
-        {
-            System.out.println(j);
-        }
+        System.out.println("Hello, World!");
     }
 }
