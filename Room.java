5e2fa2fec0e2f4072756e4d31b9dfb6976199ec2
Sam <kuhnssamuel@gmail.com>
2025-01-30 21:23:59 -0500

    added Everything.java, and getters for Room.java

--- Room.java
+++ Room.java
@@ -3,4 +3,19 @@ public class Room {
     private Room[] doors = new Room[10];
     private Enemy[] enemies = new Enemy[10];
     private Interactible[] interactibles = new Interactible[10];
+
+    Interactible getInteractible(int i)
+    {
+        return interactibles[i];
+    }
+
+    Room getRoom(int i)
+    {
+        return doors[i];
+    }
+
+    Enemy getEnemy(int i)
+    {
+        return enemies[i];
+    }
 }
