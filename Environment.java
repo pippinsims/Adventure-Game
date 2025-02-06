b1edad3bb093a3483bb9b5310599cda54afac539
Sam <kuhnssamuel@gmail.com>
2025-02-06 00:56:01 -0500

    Fixed formmatting

--- Environment.java
+++ Environment.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Environment {
 
     private static Room r0;
-    public static void main(String[] args) {
-        
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {        
         //room r0 is the current room
         r0 = generateMap();
 
-        //r0[i] refers to room i of the rooms connected to r0
+        //so r0[i] refers to room i of the rooms connected to r0
 
         Scanner scanner = new Scanner(System.in);;
         String inputStr = "";
@@ -20,27 +21,16 @@ public class Environment {
         {
             player.setActions(r0);
                 
-            //exposition////////////
-                System.out.println("You're in " + r0.getDescription() + ".");
+            //exposition
+            System.out.println("You're in " + r0.getDescription() + ".");
 
-                //list actions available
-                System.out.println("You can:");
-                String[] actionDescriptions = player.getActionDescriptions();
-                for(int i = 1; i <= actionDescriptions.length; i++)
-                {
-                    System.out.println("(" + i + ") " + actionDescriptions[i - 1]);
-                }
-            //could be a method?////
+            //list actions available
+            String[] actionDescriptions = promptList("You can:", player.getActionDescriptions());
             
             inputStr = scanner.nextLine();
             
             //we now have a running game loop.
-            
-            //SHOULD BE COMPLETELY ERROR-TRAPPED
-            if (inputNumberCheck(inputStr, actionDescriptions))
-                performAction(Integer.parseInt(inputStr), player); 
-
-           
+            performAction(forceInputToInt(inputStr, actionDescriptions), player);
         }
         scanner.close();
     }
@@ -50,118 +40,117 @@ public class Environment {
         Room r0 = new Room(new Room[3], new Enemy[1], new Interactible[2], "a dimly lit room.\nThere is a faint foul odor... \nThe patchwork on the wall depicts of a redheaded lunatic. \nLord Gareth the Mad.\nThe room is gifted");        
 
         //creating 1-door rooms for each door
-        for(int i = 0; i < r0.getNumRooms(); i++)
+        for(int i = 0; i < r0.getNumExits(); i++)
         {
             r0.setRoom(i, new Room(new Room[]{r0}));
         }
+
         for (int i = 0; i < r0.getEnemies().length; i++) 
         {
             r0.setEnemy(i, new Enemy(3));
         }
-         
-       
+
+        for(int i = 0; i < r0.getNumInteractibles(); i++)
+        {
+            //construct interactibles
+        }
+
         return r0;
     }
 
     private static void performAction(int i, Player p)
     {   
-        i--;
         Scanner kbd = new Scanner(System.in);
         //PERFORM SOME ACTION
-        switch(p.actions.get(i)){
+        switch(p.actions.get(i-1))
+        {
             case DOOR:
-                System.out.println("Which door traveler?");
-
-                String[] doorOptions = promptList("Which door traveler?", r0.getNumRooms(), "Try door &");
+                String[] doorOptions = promptList("Which door traveler?", r0.getNumExits(), "Try door &");
                 
-
-                String doahnumma = "";
+                String doahnumma = "2";
                 //we haev a glitch in el system
                 //doahnumma = kbd.nextLine();
-                doahnumma = "2";
 
-                if (inputNumberCheck(doahnumma, doorOptions))
-                {
-                    r0 = r0.getRoom(Integer.parseInt(doahnumma));
-                }
+                r0 = r0.getRoom(forceInputToInt(doahnumma, doorOptions));
+                
                 break;
+
             case FIGHT:
-                String[] fightOptions = promptList("How will you vanquish yoerer foeee??", 1, "Punch");
+                String[] fightOptions = promptList("How will you vanquish yoerer foeee??", new String[]{"Punch"});
                 String inputStr = "1"; //would actually be a call to scanner but scanner doesn't work
-                if(inputNumberCheck(inputStr, fightOptions)) //how do you want to fight
+                if(forceInputToInt(inputStr, fightOptions) != null)
                 {
                     fightOptions = promptList("Which fooeeoee meets thine bloodtherstey eyee?", r0.getEnemies().length,"Fight enemy &");
 
                     inputStr = "1"; //ditto
-                    if(inputNumberCheck(inputStr, fightOptions)) //who do you want to fight
-                    {
-                        r0.getEnemy(Integer.parseInt(inputStr) - 1).receiveDamage(1);
-                        
-                    }
+                    r0.getEnemy(forceInputToInt(inputStr, fightOptions) - 1).receiveDamage(1);
                 }
-                
+
+                break;
 
             default:
                 break;
-        
-            
-
         }
         kbd.close();
     }
 
-    /**
-     * Description of method...
-     * @param question the question that will be printed
-     * @param listSize
-     * @param listPrompts
-     * @return
-     */
     private static String[] promptList(String question, int listSize, String listPrompts)
-    {
+    {        
         String[] options = new String[listSize];
-                for (int i=0; i < listSize; i++) {
-                    String tempListPrompts = listPrompts;
-                    for(int j = 0; j < listPrompts.length(); j++)
-                    {
-                        if(tempListPrompts.charAt(j) == '&')
-                        {
-                           tempListPrompts = tempListPrompts.substring(0, j) + (i+1) + tempListPrompts.substring(j+1, tempListPrompts.length());
-                        }
-                    }
-                    options[i] = tempListPrompts;
-                    System.out.println(options[i]);
-                }
-                return options;
+        for(int i = 0; i < listSize; i++)
+        {
+            options[i] = listPrompts;
+
+            //replace '&'s with 1-based indexes
+            for(int j = 0; j < listPrompts.length(); j++)
+            {
+                if(options[i].charAt(j) == '&')
+                    options[i] = options[i].substring(0, j) + (i + 1) + options[i].substring(j + 1, options[i].length());
+            }
+        }
+
+        return promptList(question, options);
     }
 
-    private static boolean inputNumberCheck(String s, String[] options)
+    private static String[] promptList(String question, String[] listPrompts)
+    {
+        System.out.println(question);
+        
+        for(int i = 0; i < listPrompts.length; i++) 
+        {
+            System.out.println("(" + i + ") " + listPrompts[i]);
+        }
+
+        return listPrompts;
+    }
+
+    private static Integer forceInputToInt(String s, String[] options)
     {
         Scanner kbd = new Scanner(System.in);
     
-        boolean falseInput = true;
+        Integer inputInt = null;
         do
         {      
-            try {
-                Integer.parseInt(s);
-                falseInput = false;
+            try 
+            {
+                inputInt = Integer.parseInt(s);
             }   
             catch(Exception e)
             {
-                if (s.contains("fuck"))
-                {              
-                    System.out.println("Yeah okay fuck you too man, I'm just trying to do my job"); 
-                }
-                for(int i = 1; i <= options.length; i++)
-                    {
-                        System.out.println("(" + i + ") " + options[i - 1]);
-                    }
-                falseInput = true;
-                s = kbd.nextLine();
-            } 
-        } while(falseInput);
-        kbd.close();
-        return !falseInput;
-    }
+                if (s.contains("fuck"))             
+                    System.out.println("Yeah okay fuck you too man, I'm just trying to do my job."); 
 
+                for(int i = 1; i <= options.length; i++)
+                {
+                    System.out.println("(" + i + ") " + options[i - 1]);
+                }
+
+                s = kbd.nextLine(); //breaks
+            } 
+        } while(inputInt == null);
+        
+        kbd.close();
+
+        return inputInt - 1; //input is 1-based
+    }
 }
