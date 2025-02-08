e07e13c5e5fe7d96fdf126af2ed528e4de2168be
Sam <kuhnssamuel@gmail.com>
2025-02-08 16:45:37 -0500

    Restructured the code: InteractionUtil.java now holds all the printing/input methods, and is implemented in Player and Environment for frequent use. Player now holds the performAction method

--- src/Environment.java
+++ src/Environment.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
-import java.io.File;
-
-public class Environment 
+public class Environment extends InteractionUtil
 {
-    public static Scanner scanner = new Scanner(System.in);
-    private static Room r0;
+    public static Room r0;
     public static void main(String[] args) 
     {
         //room r0 is the current room
@@ -24,7 +20,7 @@ public class Environment
             {
                 savedRoom = r0;                
                 //exposition
-                slowPrintln("You're in " + r0.getDescription() + ".");
+                InteractionUtil.slowPrintln("You're in " + r0.getDescription() + ".");
             }
             
             String descriptor;
@@ -45,6 +41,7 @@ public class Environment
                 }
                 slowPrintln(descriptor);
             }
+
             if(r0.getNumEnemies() > 0)
             {
                 descriptor = "There is a ";
@@ -62,8 +59,9 @@ public class Environment
                 }
                 slowPrintln(descriptor);
             }
+
             //lists available actions, lets the player choose, then performs chosen action.
-            if(!performAction(promptList("You can:", player.getActionDescriptions()) - 1, player))
+            if(!player.performAction(promptList("You can:", player.getActionDescriptions()) - 1))
                 break;
 
             if (r0.getEnemies() != null)
@@ -98,109 +96,6 @@ public class Environment
         r0.setInteractible(2, new ViewablePicture("mad_king.txt"));
     }
 
-    private static boolean performAction(int i, Player p)
-    {   
-        switch(p.actions.get(i))
-        {
-            case DOOR:
-                r0 = r0.getRoom(promptList("Which door traveler?", r0.getNumExits(), "Try door &") - 1);
-                
-                break;
-
-            case FIGHT:
-                String[] attackTypes = new String[]{"Punch"};
-                int attackDamage = 0;
-                int chosenAttackType = promptList("How will you vanquish yoerer foeee??", attackTypes) - 1;
-                
-                int chosenEnemyIndex;
-                if(r0.getNumEnemies() > 1)
-                    chosenEnemyIndex = promptList("Which fooeeoee meets thine bloodtherstey eyee?", r0.getNumEnemies(), "Fight enemy &") - 1;
-                else
-                    chosenEnemyIndex = 0;
-
-                switch(attackTypes[chosenAttackType])
-                {
-                    case "Punch": 
-                        attackDamage = 1;
-                        System.out.println("You heave a mighty blow at the " + r0.getEnemy(chosenEnemyIndex).getModifiedDescription("sad") + " and deal a serious " + attackDamage + " damage!");
-                        break;
-                    
-                    default:
-                        break;
-                }
-                
-                if(r0.getEnemy(chosenEnemyIndex).receiveDamage(attackDamage))
-                {
-                    slowPrintln("You have murdered the " + r0.getEnemy(chosenEnemyIndex).getRandomDescription(), 250);
-                    r0.slayEnemy(chosenEnemyIndex);
-                }
-
-                break;
-
-            case INSPECT:
-                String[] interactiblesDescriptions = new String[r0.getNumInteractibles()];
-                for(int j = 0; j < interactiblesDescriptions.length; j++)
-                {
-                    interactiblesDescriptions[j] = r0.getInteractible(j).getDescription();
-                }
-                Interactible chosenInteractible = r0.getInteractible(promptList("There are a few objects in the room:", interactiblesDescriptions) - 1);
-
-                switch (chosenInteractible.getDescription()) {
-                    case "flaming stick":
-                        slowPrintln("You take a closer look at this flaming stick and you notice that it is a burning torch, providing light and warmth!");
-                        break;
-                    
-                    case "depiction": //(if description == depiction chosenInteractible must be an instance of ViewablePicture)
-                        ViewablePicture chosenPicture = (ViewablePicture)chosenInteractible;
-                        slowPrintln("You take a closer look at the depiction:\n");
-                        String s = readFile(chosenPicture.getFileName());
-                        System.out.println(s);
-                        System.out.println("Press enter to continue");
-                        scanner.nextLine();
-
-                        break;
-                
-                    default:
-                        break;
-                }
-                
-                break;
-
-            case TALK:
-                System.out.println("What do you say?");
-                String s = scanner.nextLine();
-                if(s.equals("Quit."))
-                    return false;
-                else if(s.contains("stop"))
-                    ; //here somehow perhaps make enemy react
-                else
-                    slowPrintln("Interesting...\nWell, that does nothing.");
-                                   
-                break;
-
-            case CAST:
-                String[] spellTypes = new String[]{"brain aneurysm"};
-                int spellDamage = 0;
-
-                System.out.println("Focus...");
-                System.out.print("Speak: ");
-                String spell = scanner.nextLine(); //MAYBE INPUT SUBSTRING PARSE METHOD LATER DOWN THE LINE
-                    
-                if (spell.contains("brain aneurysm"))
-                {
-                    spellDamage = 1000;
-                    //idk yet. maybe getEnemies then damage one of them?
-                }
-                break;
-
-            default:
-                break;
-        }
-
-        return true;
-    }
-
-
     //IDEALLY THERE WILL ONLY BE 1 DEFINITION OF performAction() BUT THAT IS AFTER THE Person INTERFACE
     private static void performAction(Enemy e)
     {   
@@ -214,115 +109,5 @@ public class Environment
             default:
                 break;
         }
-    }
-
-    private static int promptList(String question, int listSize, String listPrompts)
-    {        
-        String[] options = new String[listSize];
-        for(int i = 0; i < listSize; i++)
-        {
-            options[i] = listPrompts;
-
-            //replace '&'s with 1-based indexes
-            for(int j = 0; j < listPrompts.length(); j++)
-            {
-                if(options[i].charAt(j) == '&')
-                    options[i] = options[i].substring(0, j) + (i + 1) + options[i].substring(j + 1, options[i].length());
-            }
-        }
-        
-        return promptList(question, options);
-    }
-
-    private static int promptList(String question, String[] listPrompts)
-    {
-        System.out.println(question);
-        printOptions(listPrompts);
-
-        return forceInputToInt(scanner.nextLine(), listPrompts);
-    }
-
-    private static Integer forceInputToInt(String s, String[] options)
-    {    
-        Integer inputInt = null;
-        do
-        {
-            try 
-            {
-                inputInt = Integer.parseInt(s);
-            }   
-            catch(Exception e)
-            {
-                String question = "[Incorrect input!]";
-                if (s.contains("fuck"))             
-                    question = "Yeah okay fuck you too man, I'm just trying to do my job."; 
-
-                System.out.println(question);
-                printOptions(options);
-
-                s = scanner.nextLine();
-            } 
-        } while(inputInt == null);
-        return inputInt;
-    }
-
-    private static void printOptions(String[] options)
-    {
-        for(int i = 0; i < options.length; i++) 
-        {
-            System.out.println("(" + (i + 1) + ") " + options[i]);
-        }
-    }
-
-    private static void slowPrint(String output, int sleepDuration)
-    {   
-        for(int i = 0; i < output.length(); i++)
-        {   
-            try
-            {
-                if(output.charAt(i) == '\n')
-                    Thread.sleep(sleepDuration*5);
-                Thread.sleep(sleepDuration);
-            }
-            catch(Exception e) 
-            {
-                e.printStackTrace();
-            }
-
-            System.out.print(output.charAt(i));
-        }
-    }
-
-    private static void slowPrint(String output)
-    {
-        slowPrint(output, 1);
-    }
-
-    private static void slowPrintln(String output, int sleepDuration)
-    {
-        slowPrint(output+'\n', sleepDuration);
-    }
-
-    private static void slowPrintln(String output)
-    {
-        slowPrintln(output, 1); //50 for real
-    }
-
-    private static String readFile (String fileName)
-    {
-        String completeString = "";
-        try 
-        {
-            Scanner fileScanner = new Scanner(new File("./src/" + fileName));
-            while (fileScanner.hasNextLine()) {
-                completeString += fileScanner.nextLine() + '\n';
-            }
-            fileScanner.close();
-        }
-        catch (Exception e) 
-        {
-            e.printStackTrace();
-        }
-        return completeString;
     }
 }
